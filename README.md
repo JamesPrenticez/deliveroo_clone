dee8556fb3e495408c1b2607da6efe52a1560f89
James Prentice <jamesprenticez@gmail.com>
2023-04-10 19:59:12 +1200

    Initial Setup

---
+++ README.md
@@ -0,0 +1,4 @@
+https://youtu.be/taPz40VmyzQ?t=1674
+
+(Nativewind (tailwind))[https://www.nativewind.dev/quick-starts/expo]
+(React Navigation)[https://reactnavigation.org/][https://reactnavigation.(React Navigation + Native stack)org/docs/hello-react-navigation]
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeSceen = () => {
+  return (
+    <View className="bg-blue-500 flex h-screen justify-center items-center">
+      <Text className="text-red-500">HomeSceen</Text>
+    </View>
+  )
+}
+
+export default HomeSceen
