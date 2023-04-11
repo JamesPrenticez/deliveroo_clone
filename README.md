d44f8ec1a163701233d9b42534aa63783f037532
James Prentice <jamesprenticez@gmail.com>
2023-04-11 21:03:36 +1200

    Wow react native is hard going

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ https://youtu.be/taPz40VmyzQ?t=1674
 
 (Nativewind (tailwind))[https://www.nativewind.dev/quick-starts/expo]
 (React Navigation)[https://reactnavigation.org/][https://reactnavigation.(React Navigation + Native stack)org/docs/hello-react-navigation]
+(Hero Icons React Native)[https://www.npmjs.com/package/react-native-heroicons]
+(Needed to install SVG handler)[https://docs.expo.dev/versions/latest/sdk/svg/] ```npx expo install react-native-svg```
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsHorizontalIcon
+} from "react-native-heroicons/outline";
 
 const HomeSceen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  })
+
   return (
-    <View className="bg-blue-500 flex h-screen justify-center items-center">
-      <Text className="text-red-500">HomeSceen</Text>
-    </View>
+    <SafeAreaView className="pt-8 bg-white">
+      {/* Header */}
+      <View className="pb-3 flex-row items-center mx-4 space-x-2">
+        <Image
+          className="h-7 w-7 bg-gray-300 rounded-full"
+          source={{
+            uri: "https://links.papareact.com/wru"
+          }}
+        />
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <View className="flex-row">
+              <Text className="font-bold ">Current Location</Text>
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </View>
+          </View>
+          <UserIcon size={35} color="#00CCBB"/>
+      </View>
+
+        {/* Search */}
+        <Text className="w-full bg-red-500">
+          <View className="flex-row bg-gray-300">
+            <MagnifyingGlassIcon size={20} color="black"/>
+            
+            <AdjustmentsHorizontalIcon size={20} color="black"/>
+            
+            <TextInput 
+              placeholder="Resturants"
+              keyboardType="default"
+            />
+          </View>
+        </Text>
+    </SafeAreaView>
   )
 }
 
